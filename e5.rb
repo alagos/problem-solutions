ab59ccdc1ac96ff773810ce87a65dee3604594e0
Alter Lagos <alter.strife@gmail.com>
2014-04-23 18:00:57 -0300

    optimized solution

--- e5.rb
+++ e5.rb
@@ -1,20 +1,28 @@
 #!/usr/bin/env ruby
 
+require 'prime'
+
 def e5
-  result = 0
-  i = 1
-  limit = Integer(ARGV[0]) rescue 10
-  loop do
-    [*2..limit].each do |n|
-      if i % n != 0
-        break
-      elsif n == limit
-        result = i
+  limit = Integer(ARGV[0]) rescue 20
+  primes, all_primes = [], []
+  n = 2
+
+  while n <= limit do
+    if Prime.prime?(n)
+      primes.push(n)
+      all_primes.push(n)
+    else
+      primes.each do |p|
+        nt = n
+        nt /= p while nt % p == 0
+        all_primes.push(p) if nt == 1
       end
     end
-    result > 0 ? break : i+= 1
+    puts "#{n} => #{all_primes.inspect} = #{all_primes.inject(:*)} "
+    n+=1
   end
-  result
+  all_primes
 end
 
-puts e5
+result = e5
+puts "#{result.inspect} = #{result.inject(:*)}"
